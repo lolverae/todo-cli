5e2130e93d21f0db35906b12abbc7127b8a85394
lolverae <alberto.olvera.espinosa@gmail.com>
2024-08-07 22:44:13 -0600

    Add context flag to manage multiple lists

--- cmd/manageFile.go
+++ cmd/manageFile.go
@@ -6,23 +6,25 @@ import (
 	"path/filepath"
 )
 
-func ManageTasksFile(taskFile string) *os.File {
-	// Create the .cache folder if it doesn't exist
-	cacheDir := filepath.Dir(taskFile)
+func CreateTasksContext(taskFile string) *os.File {
+	completeFilePath := ".lists/" + taskFile + ".csv"
+	cacheDir := filepath.Dir(completeFilePath)
 	err := os.MkdirAll(cacheDir, os.ModePerm)
 	if err != nil {
-		log.Printf("error creating cache directory: %s", err)
+		log.Printf("error creating lists directory: %s", err)
 	}
-	_, err = os.Stat(taskFile)
+
+	_, err = os.Stat(completeFilePath)
 	if os.IsNotExist(err) {
-		file, err := os.Create(taskFile)
+		file, err := os.Create(completeFilePath)
 		log.Printf("Task file %s not found, creating...", taskFile)
 		if err != nil {
 			log.Printf("Failed to create file")
 		}
 		return file
 	}
-	file, err := os.OpenFile(taskFile, os.O_APPEND|os.O_WRONLY, 0644)
+
+	file, err := os.OpenFile(completeFilePath, os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Printf("error opening file: %s", err)
 	}
--- cmd/root.go
+++ cmd/root.go
@@ -9,18 +9,26 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var rootCmd = &cobra.Command{
-	Use:           "bin [command]",
-	Long:          "",
-	SilenceUsage:  true,
-	SilenceErrors: true,
-	PersistentPreRun: func(cmd *cobra.Command, _ []string) {
-	},
-}
+var (
+	listContext string
+	rootCmd     = &cobra.Command{
+		Use:  "todo-cli [command] [list]",
+		Long: "A CLI tool to manage todo lists",
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			listContext, _ = cmd.Flags().GetString("list")
+			if listContext != "" {
+				CreateTasksContext(listContext)
+			} else {
+				CreateTasksContext("default")
+			}
+		},
+	}
+)
 
 func Execute() {
 	rootCmd.AddCommand(done.Cmd, create.Cmd)
 	rootCmd.CompletionOptions.DisableDefaultCmd = true
+	rootCmd.PersistentFlags().StringVarP(&listContext, "list", "l", "", "Name of the task list to modify")
 	if e := rootCmd.Execute(); e != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "%v\n", e.Error())
 		os.Exit(1)
